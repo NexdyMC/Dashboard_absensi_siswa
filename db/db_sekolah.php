5bddb8c6ded197ed92305af6c9b6bbcd9013d694
NexdyMC <febripratama1786@gmail.com>
2026-02-10 07:55:27 +0700

    database sekolah connect

--- db/db_sekolah.php
+++ db/db_sekolah.php
@@ -3,7 +3,8 @@ $host = 'localhost';
 $usernmae = 'root';
 $password = '';
 $db = 'db_sekolah';
-// $koneksi = mysqli_connect ($host, $usernmae,$password,$db)
-$db_sekolah = mysqli_connect ($host, $usernmae,$password,$db)
+
+$db_sekolah = mysqli_connect ($host, $usernmae, $password, $db)
 or die (mysqli_error($db_sekolah));
+
 ?>
